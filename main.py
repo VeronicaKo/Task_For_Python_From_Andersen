de8eb099f342eeec2d15a625a24ec9df1db79f1d
VeronicaKo <VeronicaKo1987@gmail.ru>
2021-09-16 19:52:37 +0300

    improving code quality

--- main.py
+++ main.py
@@ -1,62 +1,69 @@
 import sys
 
-if __name__ == '__main__':
-    task = input('Write the task number: ')
-    try:
-        int(task)
-    except ValueError:
-        print('It is not number.')
-        raise
 
-    if task == '1':
-        word1 = input('Write a number: ')
-        try:
-            if int(word1) > 7:
-                print('Привет')
-        except ValueError:
-            print('It is not number.')
-    elif task == '2':
-        word2 = input('What is your name? ')
-        if word2 == 'Вячеслав':
-            print('Привет, Вячеслав')
-        else:
-            print('Нет такого имени')
-    elif task == '3':
+def printPrivet():
+    num1 = input('Write a number: ')
+    if num1.isdigit():
+        if int(num1) > 7:
+            print('Привет')
+    else:
+        print('It is not number.')
+
+
+def Vyacheslav():
+    name = input('What is your name? ')
+    if name == 'Вячеслав':
+        print('Привет, Вячеслав')
+    else:
+        print('Нет такого имени')
+
+
+def NumbersMultiples3():
+    try:
         word3 = [int(el) for el in input('Write an array of numbers: ').split()]
         del3 = [i for i in word3 if i % 3 == 0]
         print(del3)
-    elif task == '4':
-        # [((())()(())]]
-        s = input('Введите скобочную последовательность: ')
-        for i in s:
-            if i != '[' and i != ']' and i != ')' and i != '(':
-                print('В последовательности не только скобки!')
-                sys.exit()
+    except ValueError:
+        print('Array has not only numbers!')
 
-        counter1 = 0
-        counter2 = 0
-        for i in s:
-            if i == '(':
-                counter1 += 1
-            elif i == ')':
-                counter1 -= 1
-            elif i == '[':
-                counter2 += 1
-            elif i == ']':
-                counter2 -= 1
 
-            if counter1 < 0:
-                print('Скобочная последовательность неправильная, не хватает круглой открывающей скобки')
-            elif counter2 < 0:
-                print('Скобочная последовательность неправильная, не хватает квадратной открывающей скобки')
+def Skobki():
+    # [((())()(())]]
+    sk_pos = input('Введите скобочную последовательность: ')
+    for tek_sk in sk_pos:
+        if tek_sk != '[' and tek_sk != ']' and tek_sk != ')' and tek_sk != '(':
+            print('В последовательности не только скобки!')
+            return False
 
-        if counter1 == 0 and counter2 == 0:
-            print('Скобочная последовательность правильная')
-        elif counter1 != 0:
-            print(
-                'Скобочная последовательность неправильная. количество открывающих и закрывающих круглых скобок не равно')
-        elif counter2 != 0:
-            print(
-                'Скобочная последовательность неправильная. количество открывающих и закрывающих квадратных скобок не равно')
-    else:
-        print('We have only 4 tasks')
+    counter1 = 0
+    counter2 = 0
+    for tek_sk in sk_pos:
+        if tek_sk == '(':
+            counter1 += 1
+        elif tek_sk == ')':
+            counter1 -= 1
+        elif tek_sk == '[':
+            counter2 += 1
+        elif tek_sk == ']':
+            counter2 -= 1
+
+        if counter1 < 0:
+            print('Скобочная последовательность неправильная, не хватает круглой открывающей скобки')
+        elif counter2 < 0:
+            print('Скобочная последовательность неправильная, не хватает квадратной открывающей скобки')
+
+    if counter1 == 0 and counter2 == 0:
+        print('Скобочная последовательность правильная')
+    elif counter1 != 0:
+        print(
+            'Скобочная последовательность неправильная. количество открывающих и закрывающих круглых скобок не равно')
+    elif counter2 != 0:
+        print(
+            'Скобочная последовательность неправильная. количество открывающих и закрывающих квадратных скобок не равно')
+
+
+if __name__ == '__main__':
+    printPrivet()
+    Vyacheslav()
+    NumbersMultiples3()
+    Skobki()
